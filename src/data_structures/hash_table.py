adbaff46ce7a9a28381bc496b6bc552a7dde8dd6
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-20 13:47:34 -0400

    Added comments

--- src/data_structures/hash_table.py
+++ src/data_structures/hash_table.py
@@ -2,12 +2,16 @@
 
 class HashTable:
 
+    ''' Create a hash table with size m.'''
     def __init__(self, m):
+        # m is the size of tha array, r can be any prime number
         self.m = m
         self.r = 31
         self.hashtable = []
 
 
+    ''' Calculates hash for a string and puts the string in the respective index
+        of the hash table.'''
     def hash(s):
         h = 0 # hash
         for i in range(len(s)):
@@ -15,6 +19,7 @@ class HashTable:
         return h
 
 
+    ''' Inserts key-value pair into hash table.'''
     def put(k, v):
         index = self.hash(k)
         if self.hashtable[index][0] == None:
@@ -26,6 +31,7 @@ class HashTable:
             self.hashtable[index].append(v)
 
 
+    ''' Searches for a key in the hash table.'''
     def find(k):
         k = k.lower()
         index = self.hash(k)
@@ -36,6 +42,7 @@ class HashTable:
         return False
 
 
+    ''' Gets the value given a key.'''
     def getValue(k):
         index = self.hash(k)
         # Having a step of 2 ensures we only check values
@@ -43,9 +50,11 @@ class HashTable:
             if self.hashtable[index][i] == k:
                 # i+1 because there's a value after every key
                 return self.hashtable[index][i+1]
+        # If key is not in the hash table, return an error
         return "ERROR: Key does not exist"
 
 
+    ''' Returns an array of the keys.'''
     def keys():
         keysArray = []
         for i in range(self.m):
@@ -54,6 +63,7 @@ class HashTable:
         return keysArray
 
 
+    ''' Returns an array of the values.'''
     def values():
         valuesArray = []
         for i in range(self.m):
