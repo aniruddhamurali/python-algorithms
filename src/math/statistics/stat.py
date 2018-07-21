fb4f637e2d83aa8a67e838d913bab1c4d205052a
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-20 20:22:40 -0400

    Added comments

--- src/math/statistics/stat.py
+++ src/math/statistics/stat.py
@@ -1,19 +1,24 @@
+# Statistics class consisting of basic functions for single-variate data
 
 class stat:
 
+    ''' Calculate the mean of a dataset.'''
     def mean(data):
         return sum(data)/len(data)
 
 
+    ''' Calculate the range of a dataset.'''
     def range(data):
         return max(data) - min(data)
 
 
+    ''' Calculate the interquartile range of a dataset.'''
     def iqr(data):
         fiveNum = stat.fiveNumSummary(data)
         return fiveNum[3] - fiveNum[1]
 
 
+    ''' Calculate the standard deviation of a dataset.'''
     def stdev(data):
         m = stat.mean(data)
         answer = 0
@@ -23,10 +28,12 @@ class stat:
         return answer
 
 
+    ''' Calculate the variance of a dataset.'''
     def variance(data):
         return (stat.stdev(data))**2
 
 
+    ''' Return the median of a dataset.'''
     def median(data):
         if (len(data) % 2 == 1): return data[int((len(data)-1)/2)]
         else:
@@ -35,6 +42,7 @@ class stat:
             return (ind1+ind2)/2
 
 
+    ''' Return the mode of a dataset.'''
     def mode(data):
         freqDict = dict()
         for n in data:
@@ -44,10 +52,17 @@ class stat:
         return list(freqDict.keys())[maxFreqIndex]
 
 
+    ''' Calculate z-score given value, mean, and standard deviation.'''
     def zscore(x, u, sd):
         return (x-u)/sd
     
-    
+
+    ''' Return a five number summary of a dataset:
+        q0: minimum
+        q1: lower quartile
+        q2: median
+        q3: upper quartile
+        q4: maximum '''
     def fiveNumSummary(data):
         q0 = data[0]
         q2 = stat.median(data)
@@ -62,7 +77,8 @@ class stat:
             
         return [q0,q1,q2,q3,q4]
 
-    
+
+    ''' Determines if a dataset has outliers.'''
     def hasOutliers(data):
         count = 0
         outliers = list()
