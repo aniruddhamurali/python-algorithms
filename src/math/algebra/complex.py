a261254e084e8498eedf1d0d05188f9515607582
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-24 13:57:22 -0400

    Added complex multiplication

--- src/math/algebra/complex.py
+++ src/math/algebra/complex.py
@@ -38,8 +38,19 @@ class Complex:
         if type(c) == int or type(c) == float:
             new_complex = Complex(self.r*c, self.i*c)
         else:
-            # (a+bi)(c+di) = ac + a*di + b*ci - b*d
+            # (a+bi)(c+di) = ac + adi + bci - bd
             new_complex = Complex(self.r*c.r - self.i*c.i, self.r*c.i + self.i*c.r)
         return new_complex
 
+    def __truediv__(self, c):
+        if type(c) == int or type(c) == float:
+            new_complex = Complex(self.r/c, self.i/c)
+        else:
+            # (a+bi)/(c+di) = ((a+bi)(c-di))/((c+di)(c-di))
+            # = (ac - adi + bci + bd)/(c^2 + d^2)
+            # Real: (ac+bd)/(c^2+d^2)  Imaginary: (-ad+bc)/(c^2+d^2)
+            new_r = (self.r*c.r + self.i*c.i)/(c.r**2 + c.i**2)
+            new_i = (-1*self.r*c.i + self.i*c.r)/(c.r**2 + c.i**2)
+            new_complex = Complex(new_r, new_i)
+        return new_complex
 
