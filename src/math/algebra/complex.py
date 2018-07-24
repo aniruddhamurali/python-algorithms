a4328777ba9f2b33125324502e5537d81f26418b
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-24 14:59:35 -0400

    Added complex absolute value and comments

--- src/math/algebra/complex.py
+++ src/math/algebra/complex.py
@@ -1,10 +1,15 @@
+# Module for complex numbers
 
 class Complex:
 
+    ''' Creates a complex number. The first parameter is the real component, the second
+        parameter is the imaginary component.'''
     def __init__(self, r, i):
         self.r = r
         self.i = i
 
+
+    ''' Calling print() will display the complex number in the form a+bi.'''
     def __str__(self):
         string = ""
         if self.r == 0:
@@ -26,14 +31,23 @@ class Complex:
                     string += "-" + str(abs(self.i)) + "i"
         return string
 
+
+    ''' Adds two complex number objects; allows you to use plus sign for adding complex
+        numbers.'''
     def __add__(self, c):
         new_complex = Complex(self.r + c.r, self.i + c.i)
         return new_complex
 
+
+    ''' Subtracts two complex number objects; allows you to use minus sign for subtracting
+        complex numbers.'''
     def __sub__(self, c):
         new_complex = Complex(self.r - c.r, self.i - c.i)
         return new_complex
 
+
+    ''' Multiplies two complex number objects; allows you to use the multiplication sign
+        for multiplying complex numbers.'''
     def __mul__(self, c):
         if type(c) == int or type(c) == float:
             new_complex = Complex(self.r*c, self.i*c)
@@ -42,6 +56,9 @@ class Complex:
             new_complex = Complex(self.r*c.r - self.i*c.i, self.r*c.i + self.i*c.r)
         return new_complex
 
+
+    ''' Divides two complex number objects; allows you to use the division sign for
+        dividing complex numbers.'''
     def __truediv__(self, c):
         if type(c) == int or type(c) == float:
             new_complex = Complex(self.r/c, self.i/c)
@@ -53,4 +70,9 @@ class Complex:
             new_i = (-1*self.r*c.i + self.i*c.r)/(c.r**2 + c.i**2)
             new_complex = Complex(new_r, new_i)
         return new_complex
+
+
+    ''' Calculates the absolute value of the complex number.'''
+    def __abs__(self):
+        return pow(self.r**2 + self.i**2, .5)
 
