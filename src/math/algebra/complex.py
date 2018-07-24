b2f04617ff82e7c87270efe64c1c717e284dd351
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-24 12:04:29 -0400

    Modified __str__() and added addition and subtraction

--- src/math/algebra/complex.py
+++ src/math/algebra/complex.py
@@ -6,11 +6,30 @@ class Complex:
         self.i = i
 
     def __str__(self):
-        string = str(self.r)
-        if self.i > 0:
-            string += "+" + str(self.i)
-        elif self.i < 0:
-            string += "-" + str(abs(self.i))
-        string += "i"
+        string = ""
+        if self.r == 0:
+            if self.i != 0:
+                string += str(self.i) + "i"
+            else:
+                string += "0"
+        else:
+            string += str(self.r)
+            if self.i > 0:
+                if self.i == 1:
+                    string += "+" + "i"
+                else:
+                    string += "+" + str(self.i) + "i"
+            elif self.i < 0:
+                if self.i == -1:
+                    string += "-" + "i"
+                else:
+                    string += "-" + str(abs(self.i)) + "i"
         return string
-        
+
+    def __add__(self, c):
+        new_complex = Complex(self.r + c.r, self.i + c.i)
+        return new_complex
+
+    def __sub__(self, c):
+        new_complex = Complex(self.r - c.r, self.i - c.i)
+        return new_complex
