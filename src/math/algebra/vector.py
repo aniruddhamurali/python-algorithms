2d523d8fda90e7217e72a0733351c08d2cf604f4
Aniruddha Murali <aniruddha.murali@gmail.com>
2018-07-23 21:24:15 -0400

    Added vector module

---
+++ src/math/algebra/vector.py
@@ -0,0 +1,82 @@
+# Vector module
+# Allows you to use many functions that are similar to those of lists.
+# Also allows you to perform mathematical operations on a vector.
+
+class Vector:
+
+    ''' Create a vector by inputting an array.'''
+    def __init__(self, vector):
+        self.vector = vector
+
+
+    ''' Calling print() will display the vector itself.'''
+    def __str__(self):
+        string = "< "
+        for i in range(len(self.vector)):
+            string += str(self.vector[i])
+            if i != len(self.vector)-1:
+                string += ", "
+        string += " >"
+        return string
+
+
+    ''' Returns the number of components in the vector; allows you to use len()
+        on Vector objects.'''
+    def __len__(self):
+        return len(self.vector)
+
+
+    ''' Returns if the vector contains the given value; allows you to use the
+        'in' keyword (value in Vector).'''
+    def __contains__(self, value):
+        for i in self.vector:
+            if i == value:
+                return True
+        return False
+
+
+    ''' Returns if two Vector objects are equal; allows you to use == to compare
+        two vectors.'''
+    def __eq__(self, v):
+        if len(self.vector) != len(v.vector):
+            return False
+        for i in self.vector:
+            for j in v.vector:
+                if i != j:
+                    return False
+        return True
+
+
+    ''' Returns the component of the vector at the given index.'''
+    def get(self, index):
+        return self.vector[index]
+
+    ''' Returns the first index of a component in the vector.'''
+    def index(self, value):
+        for i in range(len(self.vector)):
+            if self.vector[i] == value:
+                return i
+
+    ''' Appends value onto the end of the vector.'''
+    def append(self, value):
+        self.vector += [value]
+
+    ''' Inserts value right before the given index.'''
+    def insert(self, index, value):
+        self.vector = self.vector[:index] + [value] + self.vector[index:]
+
+    ''' Remove a component from the vector.'''
+    def remove(self, index):
+        del self.vector[index]
+
+    ''' Removes all components of the vector.'''
+    def clear(self):
+        for i in range(len(self.vector)):
+            del self.vector[0]
+
+    ''' Sets a component of the vector with the given value at the given index.'''
+    def component(self, index, value):
+        self.vector[index] = value
+
+
+    
